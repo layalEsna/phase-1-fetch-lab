e9db492f9e75643507b8cca650038f9b1b730e40
Amene <layla_1380@yahoo.com>
2024-04-18 11:57:27 -0500

    fetching titles

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function fetchBooks() {
+function fetchBooks(callback) {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(res => {
+      return res.json()
+    })
+    .then(data => callback(data))
 }
 
 function renderBooks(books) {
@@ -12,6 +16,7 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks(renderBooks);
 });
+///////// I can see a list of Game Of Thrones titles on the webpage.
